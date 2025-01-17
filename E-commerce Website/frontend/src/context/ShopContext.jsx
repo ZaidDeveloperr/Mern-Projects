2ec12c633e16b948e3c9e1623825d910fbd6bc10
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-01-17 20:25:26 +0530

    Cart Page Done

--- E-commerce Website/frontend/src/context/ShopContext.jsx
+++ E-commerce Website/frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/assets';  // Ensure products is properly imported
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -11,7 +12,8 @@ const ShopContextProvider = (props) => {
   const [search, setSearch] = useState('');
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({})
-
+  const navigate = useNavigate()
+useNavigate
   const addToCart = async (itemId, size) => {
 
     if(!size){
@@ -52,6 +54,30 @@ const ShopContextProvider = (props) => {
     return totalCount
   }
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems)
+    cartData[itemId][size] = quantity
+
+    setCartItems(cartData)
+  }
+
+  const getCartAmount = () => {
+    let totalAmount = 0
+    for (const items in cartItems){
+      let itemInfo = products.find((product) => product._id === items)
+      for(const item in cartItems[items]){
+        try {
+          if(cartItems[items][item] > 0){
+            totalAmount += itemInfo.price * cartItems[items][item]
+          }
+        } catch (error) {
+          
+        }
+      }
+    }
+    return totalAmount
+  }
+
   const value = {
     products, // Products should be available here for any component consuming this context
     currency, 
@@ -62,7 +88,10 @@ const ShopContextProvider = (props) => {
     setShowSearch,
     cartItems,
     addToCart,
-    getCartCount
+    getCartCount,
+    updateQuantity,
+    getCartAmount,
+    navigate
   };
 
   return (
