4fc6d66847ff7d716d78ea5b13b7d169d47d06ce
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-01-23 14:37:01 +0530

    Backend Done

---
+++ E-commerce Website/backend/middleware/adminAuth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken"
+
+
+const adminAuth = async(req,res,next) => {
+    try {
+        const { token } = req.headers // Extracts the 'token' from the request headers.
+
+        if(!token) {
+            return res.json({success:false, message: "Not Authorized, Login Again"})
+        }
+
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET) // Verifies the token using the JWT secret key stored in environment variables.
+
+        if(token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD){ // Compares the decoded token with admin's email + password
+            return res.json({success:false, message: "Not Authorized, Login Again"})
+        }
+        next() // Calls the next middleware if the token is valid and matches the admin credentials.
+
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export default adminAuth
--- E-commerce Website/backend/models/productModel.js
+++ E-commerce Website/backend/models/productModel.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose"; //Importing Mongoose
 const productSchema = new mongoose.Schema({ // Defining the Schema Structure
     name: { type: String, required: true },
     description: { type: String, required: true },
-    price: { type: String, required: true },
-    image: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
     category: { type: String, required: true },
     subCategory: { type: String, required: true },
-    sizes: { type: String, required: true },
-    bestseller: { type: String, required: true },
-    date: { type: String, required: true }
+    sizes: { type: Array, required: true },
+    bestseller: { type: Boolean},
+    date: { type: Number, required: true }
 })
 
 const productModel = mongoose.model.product || mongoose.model("product", productSchema)
