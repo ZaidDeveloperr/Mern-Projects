164d0d47e0aeb15c622011146439b2ccb9154500
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-01-27 23:18:25 +0530

    Code Updated

--- E-commerce Website/backend/server.js
+++ E-commerce Website/backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/ProductRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 const app = express(); // Create an Express app
 const port = process.env.PORT || 4000; // Set port
@@ -20,6 +21,7 @@ app.use(cors()); // Middleware to enable CORS for all incoming requests
 // API Endpoints
 app.use('/api/user', userRouter); // User-related routes
 app.use('/api/product', productRouter)
+app.use('/api/cart', cartRouter)
 
 app.get('/', (req, res) => { // Simple root route
     res.send("API Working"); // Send confirmation message
