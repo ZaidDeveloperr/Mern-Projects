da97b446fba1d49c6201b893656b6a2598f60e6e
zaid-devel <184748306+zaid-devel@users.noreply.github.com>
2025-02-16 21:11:14 +0530

    Home Page Completed

---
+++ ChatBot/src/layouts/rootlayout/Rootlayout.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './rootLayout.css'
+import { Link, Outlet } from 'react-router-dom'
+import { ClerkProvider, SignedIn, UserButton } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+} // --------> Used in RootLayout.jsx
+
+const Rootlayout = () => {
+  return (
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <div className='rootLayout'>
+        <header>
+          <Link to='/' className='logo'>
+            <img src="/logo.png" alt="Logo" />
+            <span>AI</span>
+          </Link>
+          <div className="user">
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </ClerkProvider>
+  )
+}
+
+export default Rootlayout
